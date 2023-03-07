e3b678ab5875df00f88ea92b301b179afcb2ba66
nsavi <112178048+nsavi@users.noreply.github.com>
2023-03-07 19:24:33 +0530

    Add files via upload

---
+++ ClassDemo.java
@@ -0,0 +1,19 @@
+class Box{
+	int length;
+	int breadth;
+	int height;
+}
+
+public class ClassDemo
+{
+	public static void main(String[] args)
+	{
+		//Box black box
+		//black box = new Box();
+		Box blackbox = new Box();
+		blackbox.length = 10;
+		blackbox.breadth =8;
+		
+	System.out.println(blackbox.length);
+	}
+}
