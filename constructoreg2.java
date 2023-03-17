669bd602c13fe7ef94ff14530473af066d5d314d
nsavi <112178048+nsavi@users.noreply.github.com>
2023-03-17 09:40:47 +0530

    Add files via upload

---
+++ constructoreg2.java
@@ -0,0 +1,15 @@
+class Bike1
+{
+	Bike1()
+	{
+		System.out.print("Bike is created");
+	}
+}
+
+public class constructoreg2
+{
+	public static void main(String[] args)
+	{
+		Bike1 b=new Bike1();
+	}
+}
