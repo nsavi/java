9acb16f4b78c898d6020df08e0922966426a1488
nsavi <112178048+nsavi@users.noreply.github.com>
2023-05-04 10:21:17 +0530

    Add files via upload

---
+++ Teststringcomparison3.java
@@ -0,0 +1,12 @@
+class Teststringcomparison3
+{
+	public static void main(String args[])
+	{
+		String s1 = "Sachin";
+		String s2 = "Sachin";
+		String s3 = new String("Sachin");
+		String s4 = "Saurav";
+		System.out.println(s1==s2);
+		System.out.println(s1==s3);
+	}
+}
