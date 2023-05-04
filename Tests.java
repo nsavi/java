deb74f2ec0e52f15bd925187894f2629c79eea96
nsavi <112178048+nsavi@users.noreply.github.com>
2023-05-04 11:08:41 +0530

    Add files via upload

---
+++ Tests.java
@@ -0,0 +1,16 @@
+class Tests
+{
+	public static void main(String args[])
+	{
+		String myStr = "Hello";
+		System.out.println(myStr.replace('l','p'));
+		
+		String myStr1 = "    Hello World!    ";
+		System.out.println(myStr1);
+		System.out.println(myStr1.trim());
+		
+		String txt = "cheNnaI";
+		System.out.println(txt.toUpperCase());
+		System.out.println(txt.toLowerCase());
+	}
+}
