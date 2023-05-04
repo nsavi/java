445af216c6ffccfaf6c8e7e86c40ec2347174ea6
nsavi <112178048+nsavi@users.noreply.github.com>
2023-05-04 10:42:31 +0530

    Add files via upload

---
+++ TestSttringConcatenation1.java
@@ -0,0 +1,11 @@
+class TestSttringConcatenation1
+{
+	public static void  main(String args[])
+	{
+		String s = "Mother" + "Terasa";
+		System.out.println(s);
+		
+		String s1 = 50+30+"Sachin"+40+40;
+		System.out.println(s1);
+	}
+}
