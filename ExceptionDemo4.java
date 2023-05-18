5a1f6693fadd744ed3a06f1a492977c8f04362af
nsavi <112178048+nsavi@users.noreply.github.com>
2023-05-18 10:15:36 +0530

    Add files via upload

---
+++ ExceptionDemo4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExceptionDemo4
+{
+	public static void main(String args[])
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter student mark1");
+		int mark = reader.nextInt();
+		System.out.println("You entered Studentmark as "+mark);
+	}
+}
