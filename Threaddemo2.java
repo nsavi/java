97273398c0cfef4e2589991312ff08a1b2b505b7
nsavi <112178048+nsavi@users.noreply.github.com>
2023-05-19 09:02:05 +0530

    Add files via upload

---
+++ Threaddemo2.java
@@ -0,0 +1,24 @@
+class Example1 extends Thread
+{
+	public void run()
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("child thread"+i);
+		}
+	}
+}
+
+public class Threaddemo2
+{
+	public static void main(String[] args)
+	{
+		Example1 obj = new Example1();
+		obj.start();
+							System.out.println(obj.getState());
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("main thread" +i);
+		}
+	}
+}
